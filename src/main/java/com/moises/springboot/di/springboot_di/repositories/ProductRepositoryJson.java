163895a3b89cfaca03ee437d4f6cf9fc63acb423
Moises Silva Cardenas <moises.silva@et.bo>
2024-06-18 17:59:48 -0400

    62. Creando clase que lee los datos de un archivo json

---
+++ src/main/java/com/moises/springboot/di/springboot_di/repositories/ProductRepositoryJson.java
@@ -0,0 +1,44 @@
+package com.moises.springboot.di.springboot_di.repositories;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.moises.springboot.di.springboot_di.models.Product;
+
+public class ProductRepositoryJson implements ProductRepository{
+
+    private List<Product> list;
+    
+    public ProductRepositoryJson() {
+        Resource resource = new ClassPathResource("json/product.json");
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            list = Arrays.asList(objectMapper.readValue(resource.getFile(), Product[].class));
+        } catch (StreamReadException e) {
+            e.printStackTrace();
+        } catch (DatabindException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public List<Product> findAll() {
+        return list;
+    }
+
+    @Override
+    public Product findbyId(Long id) {
+        return null;
+    }
+
+}
